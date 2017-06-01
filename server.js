aaa006cfac3116c8357a4a55a9264df171f76dec
thehatter <andrewlutenko@gmail.com>
2017-06-01 22:31:12 +0300

    Add some comments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
+  // get ip adress
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(req.headers);
   console.log(ip);
